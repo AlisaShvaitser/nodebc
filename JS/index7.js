5c362052e3da64a30c04b4639b1aa66ffc2eff31
AlisaShvaitser <alisa.spbgau@yandex.ru>
2020-01-24 16:51:55 -0800

    Create index7.js

---
+++ JS/index7.js
@@ -0,0 +1,12 @@
+let first_name;
+let last_name;
+let age;
+let dateOfBirth;
+let address;
+
+first_name = 'Alisa';
+last_name = 'Shvaitser';
+age = '39';
+dateOfBirth = '01/22/1981';
+address = 'Vancouver';
+console.log(first_name);
