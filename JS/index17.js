5479621dd159ba4cbbd642bf9445d138c07749f3
AlisaShvaitser <alisa.spbgau@yandex.ru>
2020-01-31 18:07:52 -0800

    Create index17.js

---
+++ JS/index17.js
@@ -0,0 +1,8 @@
+let seriesName='Game of Thrones'
+let dragon1= 'Drogon'
+let coma = ','
+let dragon2= 'Rhaegal'
+let dragon3= 'Viserion'
+
+console.log ('In', seriesName, 'the dragons name are:',dragon1, coma, dragon2, '&',dragon3)
+//In Game of Thrones the dragons name are: Drogon, Rhaegal & Viserion
