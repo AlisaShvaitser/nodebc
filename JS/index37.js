144ad4607238b07eb54526fe7c8383a2f7a055b9
AlisaShvaitser <alisa.spbgau@yandex.ru>
2020-01-31 23:37:30 -0800

    Create index37.js

---
+++ JS/index37.js
@@ -0,0 +1,8 @@
+let average = 6;
+let studentAverage = 4;
+
+if (average === studentAverage) {console.log ("True")}
+else
+{console.log ("False")}
+
+console.log ("This student is missing "+ (average - studentAverage) +" points to pass")
