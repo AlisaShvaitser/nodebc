2389b3aca4dd35edbc6abe6296e1b5ddf59ba138
AlisaShvaitser <alisa.spbgau@yandex.ru>
2020-01-31 23:37:54 -0800

    Create index32.js

---
+++ JS/index32.js
@@ -0,0 +1,7 @@
+let height = 10;
+let base = 4;
+let perimiterRectangular = (height + base) * 2 ;
+let areaRectangular = (height * base) ;
+console.log ( "perimiter of the rectangular " + perimiterRectangular);
+console.log ("area of the rectangular " + areaRectangular);
+
