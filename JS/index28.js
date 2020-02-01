f1fe0403590898bb31227891b488ff158f4e23d9
AlisaShvaitser <alisa.spbgau@yandex.ru>
2020-01-31 18:06:38 -0800

    Create index28.js

---
+++ JS/index28.js
@@ -0,0 +1,28 @@
+let number =9;
+let number1 =1;
+let number2 =2;
+let number3 =3;
+let number4 =4;
+let number5 =5;
+let number6 =6;
+let number7 =7;
+let number8 =8;
+let number9 =9;
+let number10 =10;
+let messageTimes= " times "
+let messageEquals= " equals "
+
+const result1 = number * number1;
+const result2 = number * number2;
+const result3 = number * number3;
+const result4 = number * number4;
+const result5 = number * number5;
+const result6 = number * number6;
+const result7 = number * number7;
+const result8 = number * number8;
+const result9 = number * number9;
+const result10 = number * number10;
+
+console.log( number10 +messageTimes + number + messageEquals + result10);
+
+//10 times 9 equals 90
