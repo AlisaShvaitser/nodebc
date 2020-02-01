bb3b2531b4452700bba69ac66e50ace54420722f
AlisaShvaitser <alisa.spbgau@yandex.ru>
2020-01-31 18:06:47 -0800

    Create index27.js

---
+++ JS/index27.js
@@ -0,0 +1,9 @@
+let firstNumber = 2;
+let secondNumber= 5;
+
+//const result = firstNumber + secondNumber;
+//const result = firstNumber - secondNumber;
+//const result = firstNumber * secondNumber;
+const result = firstNumber % secondNumber;
+//const result = firstNumber / secondNumber;
+console.log(result);
