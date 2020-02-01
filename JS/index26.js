8d5f15c3c96ef408cf2393e98173dbba3642193b
AlisaShvaitser <alisa.spbgau@yandex.ru>
2020-01-31 18:06:43 -0800

    Create index26.js

---
+++ JS/index26.js
@@ -0,0 +1,15 @@
+let name = "Winnipeg";
+let counrtyName = "Canada";
+let provinceBC = false;
+let provinceManitoba = true;
+let population =  undefined; 
+let penguins = null;
+
+console.log('name', name);
+
+console.log(typeof name); // string
+console.log(typeof counrtyName); // string
+console.log(typeof provinceBC); // boolean
+console.log(typeof provinceManitoba); // boolean
+console.log(typeof population); // undefined
+console.log(typeof fpenguins); // object
