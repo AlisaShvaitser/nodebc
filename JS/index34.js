d66649faf777f59c30576223b693c22fa3f17a53
AlisaShvaitser <alisa.spbgau@yandex.ru>
2020-01-31 23:37:45 -0800

    Create index34.js

---
+++ JS/index34.js
@@ -0,0 +1,12 @@
+
+let IncomeObjective = 800;
+let TaxesObjective = 400;
+let BonusObjective = 599;
+
+let income= 1000;
+let taxes= 500;
+let revenue= 600;
+
+console.log(IncomeObjective < income); //800<1000 T
+console.log(TaxesObjective > taxes); //400>500 F
+console.log(BonusObjective < revenue); //600>599 T
