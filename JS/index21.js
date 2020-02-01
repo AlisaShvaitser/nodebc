b48611d0e6f8dd0d49051324c5fd93cbfffe331a
AlisaShvaitser <alisa.spbgau@yandex.ru>
2020-01-31 18:07:15 -0800

    Create index21.js

---
+++ JS/index21.js
@@ -0,0 +1,11 @@
+let age = 39;
+let phone = 77877208;
+let streetName = " Water";
+let streetNumber = 66;
+let Message1 ="I\'m " + age + " years old"
+let Message2 ="My phone number is" + phone;
+let Message3 ="I live on " + streetNumber  + streetName;
+
+console.log(Message1);
+console.log(Message2);
+console.log(Message3);
