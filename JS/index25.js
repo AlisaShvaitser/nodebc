a72c238eeede83861d7c4df2f53bcf4c75958f46
AlisaShvaitser <alisa.spbgau@yandex.ru>
2020-01-31 18:07:11 -0800

    Create index25.js

---
+++ JS/index25.js
@@ -0,0 +1,15 @@
+let superHeroName = "Batman";
+let name = "Bruce Wayne";
+let fly = false;
+let hasBatmobile = true;
+let life =  undefined; //"between 0 and 100"
+let freeTime = null;
+
+console.log('superHeroName', superHeroName);
+
+console.log(typeof superHeroName); // string
+console.log(typeof name); // string
+console.log(typeof fly); // boolean
+console.log(typeof hasBatmobile); // boolean
+console.log(typeof life); // undefined
+console.log(typeof freeTime); // object
