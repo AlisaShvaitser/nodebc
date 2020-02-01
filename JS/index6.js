2b26f1faed381b4f8c0f15ccef2652e55d8610ca
AlisaShvaitser <alisa.spbgau@yandex.ru>
2020-01-31 18:06:20 -0800

    Update index6.js

--- JS/index6.js
+++ JS/index6.js
@@ -9,6 +9,5 @@ last_name = 'Shvaitser';
 age = '39';
 dateOfBirth = '01/22/1981';
 address = 'Vancouver';
-console.log(first_name);
+console.log('Name', first_name);
 
-/*Refactor the code to show the variable name and value using console.log()*/
