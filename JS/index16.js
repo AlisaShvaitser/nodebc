b75a43e31c9c0ce6e30fbd7ff161eeeb764d234a
AlisaShvaitser <alisa.spbgau@yandex.ru>
2020-01-31 18:07:47 -0800

    Create index16.js

---
+++ JS/index16.js
@@ -0,0 +1,4 @@
+var bestStudentName='Every body';
+console.log ('The best class student is', bestStudentName)
+
+//Concatenation is the process of appending one string to the end of another string. 
