5b1550ba2a2b045da53fbb0cdc0d900aa5cc8059
AlisaShvaitser <alisa.spbgau@yandex.ru>
2020-01-31 18:07:19 -0800

    Create index22.js

---
+++ JS/index22.js
@@ -0,0 +1,11 @@
+let done = true;
+let married = false;
+let logged = false;
+let likesJS = true;
+
+console.log(done);
+console.log(married);
+console.log(logged);
+console.log (likesJS);
+console.log('logged');
+console.log("married");
