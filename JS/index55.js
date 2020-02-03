747b34bf343ece95e27b84f0644383416b6033dd
AlisaShvaitser <alisa.spbgau@yandex.ru>
2020-02-02 20:08:26 -0800

    Create index55.js

---
+++ JS/index55.js
@@ -0,0 +1,13 @@
+let firstNumber = 5;
+let secondNumber = 5;
+
+/*if (firstNumber > secondNumber) {
+    console.log("Number " + firstNumber +  " is bigger than " + secondNumber);}
+
+else {
+     console.log("Number " + secondNumber +  " is bigger than " + firstNumber);} */
+
+
+let message = ( firstNumber > secondNumber ) ? 'is bigger ' : 'is not bigger';
+
+console.log(message);
