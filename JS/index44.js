ca15d98f64deba64734a24bfa20944c0b399c6fb
AlisaShvaitser <alisa.spbgau@yandex.ru>
2020-02-02 20:07:39 -0800

    Create index44.js

---
+++ JS/index44.js
@@ -0,0 +1,11 @@
+let text = "HELLO";
+console.log (text);
+const firstChar = text.charAt(0); 
+const secondChar = text.charAt(1); 
+const thirdChar = text.charAt(2); 
+const fourChar = text.charAt(3); 
+const fifthChar = text.charAt(4); 
+
+console.log ((fifthChar).concat (fourChar,thirdChar,secondChar,firstChar )); 
+console.log ((fifthChar.toLowerCase()).concat (fourChar.toLowerCase(),thirdChar.toLowerCase(),secondChar.toLowerCase(),firstChar.toLowerCase() )); 
+//const result =  + ;
