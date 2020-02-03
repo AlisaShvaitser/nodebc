0bd3607e8655fe92f00c1a28debf26bbc7d2272e
AlisaShvaitser <alisa.spbgau@yandex.ru>
2020-02-02 20:07:25 -0800

    Create index41.js

---
+++ JS/index41.js
@@ -0,0 +1,9 @@
+let text1 = "HELLO ";
+let text2 = "javascript ";
+let text3 = "WORLD"
+
+console.log(text1.toLowerCase()); 
+console.log(text2.toUpperCase()); 
+console.log(text3.toLowerCase());
+
+console.log(text1.toLowerCase().concat ( text2.toUpperCase(), text3.toLowerCase() ));
