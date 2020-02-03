c257de52092f36be8548a9218effc6b5cf707ef2
AlisaShvaitser <alisa.spbgau@yandex.ru>
2020-02-02 20:08:22 -0800

    Create index54.js

---
+++ JS/index54.js
@@ -0,0 +1,6 @@
+let username = "pepe2017";
+let password = "12346";
+
+if (username == "pepe2017" , password == "12345") {
+    console.log( "Logged in user, show user home page.")}
+    else { console.log ("Sorry, there has been a problem, please try it again.")};
