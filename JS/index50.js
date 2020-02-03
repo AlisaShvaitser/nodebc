9c9adf7a8ebd4e63573cfd4db27f64f25672b9f3
AlisaShvaitser <alisa.spbgau@yandex.ru>
2020-02-02 20:08:05 -0800

    Create index50.js

---
+++ JS/index50.js
@@ -0,0 +1,9 @@
+let number = 10;
+
+/*if (number = 2, 4, 6, 8, 10) {
+    console.log( number + " is an even number");
+  } */
+
+  if (number %2 == 0) {
+    console.log( number + " is an even number");
+  } 
