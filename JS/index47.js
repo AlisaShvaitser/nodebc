2888681d5779de591ce8a79cef4234ec88d44d40
AlisaShvaitser <alisa.spbgau@yandex.ru>
2020-02-02 20:07:52 -0800

    Create index47.js

---
+++ JS/index47.js
@@ -0,0 +1,5 @@
+let students = "Alisa, Otto, Teresa, Fred, Alex"
+let studentsArray = students.split(',');
+console.log(studentsArray);
+console.log("Students names: " + students);
+console.log("Students names: " + studentsArray);
