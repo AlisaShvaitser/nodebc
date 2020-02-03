3c86c12abc8c5a1a2d8805bd916f87ccdb8ced3e
AlisaShvaitser <alisa.spbgau@yandex.ru>
2020-02-02 20:08:31 -0800

    Create index56.js

---
+++ JS/index56.js
@@ -0,0 +1,10 @@
+let number = 10;
+
+  /*if (number %2 == 0) {
+    console.log( number + " is an even number");
+  } else { console.log (number + " is an odd number")} */
+
+
+  let message = ( number %2 == 0) ? 'is an even number' : 'is an odd number';
+
+console.log(message);
