51a0c6111eff48a28de6aab3e3a45fb9a8849fb7
AlisaShvaitser <alisa.spbgau@yandex.ru>
2020-02-26 19:26:14 -0800

    Create array.js

---
+++ battleship/array.js
@@ -0,0 +1,11 @@
+var array_counter = function (array){
+return "massiv has " + array.length + " elements";
+};
+//console.log (array_counter([1,7,8,99]));
+
+module.exports = array_counter;
+
+var multiply = function (x,y) {
+    return `$(x) multiply $(y) equals $(x*y)`;
+}
+var some_value = 451;
