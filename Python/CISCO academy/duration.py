14355ddcbaa399ed0fe010d196f84d8593b7a8b9
AlisaShvaitser <alisa.spbgau@yandex.ru>
2020-07-07 17:58:06 -0700

    Cisco

--- Python/CISCO academy/duration.py
+++ Python/CISCO academy/duration.py
@@ -2,12 +2,9 @@ hour = int(input("Starting time (hours): "))
 mins = int(input("Starting time (minutes): "))
 dura = int(input("Event duration (minutes): "))
 endHour = int ((hour *60 + mins + dura)//60)
-for endHour:
-    if endHour >=24:
-        endHour=endHour-24
-   
-
-
+while endHour >=24:
+    endHour=endHour-24
+    
 endMin = int ((hour *60 + mins + dura)%60)
 
 print ( "If an event starts at", hour, ":", mins, "and lasts", dura, "minutes, it will end at", endHour, ":", endMin, ".")
---
+++ Python/CISCO academy/my.py
@@ -0,0 +1,14 @@
+userWord = input ("Please, enter a word: ")
+# userWord = userWord.uppe
+for letter in userWord:
+    if letter == "A":
+        continue
+    if letter == "O":
+        continue
+    if letter == "U":
+        continue
+    if letter == "E":
+        continue
+    if letter == "I":
+        continue
+    print (letter)
